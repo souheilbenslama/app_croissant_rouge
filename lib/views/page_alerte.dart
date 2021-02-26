f09cf9458160451c38ea4a154edef55cdac3d027
missaouiamal <amal.missaoui@insat.u-carthage.tn>
2021-02-26 21:02:31 +0100

    alert page with bad design

---
+++ lib/views/page_alerte.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class PageAlerte extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent[700],
+        title: Text('Alert Page'),
+      ),
+      backgroundColor: Colors.redAccent[700],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            width: 410,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                IconButton(
+                  iconSize: 35,
+                  icon: Icon(
+                    Icons.account_circle,
+                    color: Colors.black,
+                  ),
+                ),
+                IconButton(
+                  iconSize: 35,
+                  icon: Icon(
+                    Icons.settings,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          RaisedButton(
+              color: Colors.red[50],
+              onPressed: () {},
+              child: Text(
+                'Alerter',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 80,
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  side: BorderSide(color: Colors.black))),
+          Container(
+            width: 410,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Contacter-nous',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Documentation',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
