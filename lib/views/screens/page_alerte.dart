ed447ed1736f25090b98f59158c2d9713120b638
missaouiamal <amal.missaoui@insat.u-carthage.tn>
2021-03-17 17:19:43 +0100

    Pdf Done

--- lib/views/screens/page_alerte.dart
+++ lib/views/screens/page_alerte.dart
@@ -1,11 +1,13 @@
 import 'package:app_croissant_rouge/views/widgets/customized_dialog.dart';
 import 'package:app_croissant_rouge/views/widgets/notification_dialog.dart';
+import '../widgets/pdf_viewer_from_asset.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 
 class PageAlerte extends StatelessWidget {
   Widget popupMenuButton() {
     return PopupMenuButton<String>(
+      //PopMenuButton because we need a menu for the settings
       icon: Icon(
         Icons.settings,
         color: Colors.black,
@@ -36,6 +38,7 @@ class PageAlerte extends StatelessWidget {
         actions: [
           // Used the actions to have the icons of the "App Bar" aligned in the same line
           IconButton(
+            // When the icon pressed it'll take as to the map page
             iconSize: 35,
             icon: Icon(
               Icons.location_on,
@@ -52,6 +55,7 @@ class PageAlerte extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
                 IconButton(
+                  // This icon button when pressed it'll take as to the signin or to the profile page if the secouriste is connected
                   onPressed: () {
                     Navigator.of(context).pushNamed('/signIn');
                   },
@@ -62,6 +66,7 @@ class PageAlerte extends StatelessWidget {
                   ),
                 ),
                 Padding(
+                  // The padding contains the the settings icon and its functions : PopUpMenuButton
                   padding: const EdgeInsets.only(
                     right: 8.0,
                     bottom: 4.0,
@@ -87,6 +92,8 @@ class PageAlerte extends StatelessWidget {
             ),
           ),
           RaisedButton(
+            // Don't like the design : needs changing
+            // Alerter when pressed will take us to the questions page
             color: Colors.redAccent[700],
             onPressed: () {
               Navigator.of(context).pushNamed('/options');
@@ -98,6 +105,7 @@ class PageAlerte extends StatelessWidget {
                 fontSize: 80,
               ),
             ),
+            // we used the option shape here so the button cand have a rounded shape
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20.0),
               side: BorderSide(color: Colors.black),
@@ -126,13 +134,35 @@ class PageAlerte extends StatelessWidget {
                 ),
                 FlatButton(
                   onPressed: () {
-                    showDialog(
+                    // To see if the notification widget works or not
+                    /*showDialog(
                       context: context,
                       builder: (BuildContext context) {
                         return NotificationDialog();
                       },
+                    );*/
+
+                    // This Method will show the pdf without creating a widget => a simple pdf transition
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => Scaffold(
+                          appBar: AppBar(
+                            title: const Text('Documentation'),
+                            backgroundColor: Colors.redAccent[700],
+                          ),
+                          body: PDF().fromAsset('assets/file/dummy.pdf'),
+                        ),
+                      ),
                     );
-                    //return PDF().fromAsset('assets/file/fichier.pdf');
+                    /* Navigator.push(
+                      context,
+                      MaterialPageRoute<dynamic>(
+                        builder: (_) => PDFViewerFromAsset(
+                          pdfAssetPath: 'assets/file/dummy.pdf',
+                        ),
+                      ),
+                    );*/
                   },
                   child: Text(
                     'Documentation',
