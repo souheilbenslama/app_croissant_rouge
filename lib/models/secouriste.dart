8d7db14bb5e2a8d90f91666baac737220d2f545f
wafaselmi <57751784+wafaselmi@users.noreply.github.com>
2021-03-17 19:31:07 +0100

    disponibility

---
+++ lib/models/secouriste.dart
@@ -0,0 +1,18 @@
+class Secouriste {
+  final int id;
+  final String imgUrl;
+  final String name;
+  final String email;
+  final int age;
+  bool isFree;
+  final int phone;
+  Secouriste({
+    this.id, 
+    this.imgUrl,
+    this.name,
+    this.email,
+    this.age,
+    this.isFree,
+    this.phone,
+  })
+}
