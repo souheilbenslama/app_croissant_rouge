bc85a7360eb98b82ce0d4f4e909affcd2d8eae1a
wafaselmi <57751784+wafaselmi@users.noreply.github.com>
2021-03-20 19:51:39 +0100

    changed the updateDisponibility

---
+++ lib/models/accident.dart
@@ -0,0 +1,24 @@
+import 'package:app_croissant_rouge/model/locationData.dart';
+
+class Accident {
+  int id;
+  int id_temoin;
+  int id_secouriste;
+  String protectionDesc;
+  String respirationDesc;
+  String hemorragieDesc;
+  String conscienceDesc;
+  String status;
+  LocationData localisation;
+
+  Accident(
+      {this.id,
+      this.id_secouriste,
+      this.id_temoin,
+      this.conscienceDesc,
+      this.hemorragieDesc,
+      this.localisation,
+      this.protectionDesc,
+      this.respirationDesc,
+      this.status});
+}
