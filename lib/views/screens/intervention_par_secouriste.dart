2ff345a8a55ca1644e5ed1b1f37f1d6bab8f0d7c
missaouiamal <amal.missaoui@insat.u-carthage.tn>
2021-05-08 21:24:19 +0100

    new format

--- lib/views/screens/intervention_par_secouriste.dart
+++ lib/views/screens/intervention_par_secouriste.dart
@@ -1,14 +1,20 @@
+import '../../models/secouriste.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:get/get.dart';
 
 class InterventionParSecouriste extends StatefulWidget {
+  final Secouriste secouriste;
+  InterventionParSecouriste({Key key, @required this.secouriste})
+      : super(key: key);
   @override
   _InterventionParSecouristeState createState() =>
       _InterventionParSecouristeState();
 }
 
 class _InterventionParSecouristeState extends State<InterventionParSecouriste> {
+  var makeAdmin = false;
+  var active = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,37 +22,133 @@ class _InterventionParSecouristeState extends State<InterventionParSecouriste> {
         backgroundColor: Colors.redAccent[700],
         title: Text("listeInter".tr),
       ),
-      body: Container(
-        child: ListView.builder(
-          itemCount: 3,
-          itemBuilder: (context, index) {
-            return Card(
-              color: Colors.white,
-              elevation: 3,
-              child: ListTile(
-                title: Column(
-                  children: [
-                    Text(
-                      "ariana".tr,
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        color: Colors.grey[800],
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      DateFormat.yMMMMEEEEd().format(DateTime.now()),
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
+      body: Column(
+        children: <Widget>[
+          Card(
+            color: Colors.white,
+            elevation: 3,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Nom et prenom : ${widget.secouriste.name.toString()}",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-            );
-          },
-        ),
+                Text(
+                  "Age : ${widget.secouriste.age.toString()}",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "E-mail : ${widget.secouriste.email.toString()}",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "Numero : ${widget.secouriste.phone.toString()}",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "Adresse : ${widget.secouriste.address.toString()}",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "Code de verification : 156325",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        IconButton(
+                          iconSize: 30,
+                          color: makeAdmin ? Colors.green : Colors.red,
+                          icon: Icon(Icons.person),
+                          onPressed: () {
+                            setState(() {
+                              makeAdmin = !makeAdmin;
+                            });
+                          },
+                        ),
+                        Text(
+                          makeAdmin ? "Admin" : "Normal User",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: makeAdmin ? Colors.green : Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        IconButton(
+                          iconSize: 30,
+                          color: active ? Colors.green : Colors.red,
+                          icon: Icon(active ? Icons.alarm_on : Icons.alarm_off),
+                          onPressed: () {
+                            setState(() {
+                              active = !active;
+                            });
+                          },
+                        ),
+                        Text(
+                          active ? "Activé" : "Desactivé",
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: active ? Colors.green : Colors.red,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: 300,
+            child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Colors.white,
+                  elevation: 3,
+                  child: ListTile(
+                    title: Column(
+                      children: [
+                        Text(
+                          "ariana".tr,
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.grey[800],
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          DateFormat.yMMMMEEEEd().format(DateTime.now()),
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/views/screens/liste_secouristes.dart
+++ lib/views/screens/liste_secouristes.dart
@@ -1,4 +1,5 @@
 import 'package:app_croissant_rouge/models/secouriste.dart';
+import 'package:app_croissant_rouge/views/screens/intervention_par_secouriste.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lite_rolling_switch/lite_rolling_switch.dart';
@@ -13,29 +14,6 @@ class ListeSecouristes extends StatefulWidget {
 class _ListeSecouristesState extends State<ListeSecouristes> {
   @override
   Widget build(BuildContext context) {
-    /*List<ItemModel> itemData = <ItemModel>[
-      ItemModel(
-        isFree: widget.secouristes[0].isFree,
-        headerValue: widget.secouristes[1].name.toString(),
-        expandedValue: widget.secouristes[1].email.toString(),
-      ),
-      ItemModel(
-        isFree: widget.secouristes[1].isFree,
-        headerValue: widget.secouristes[1].name.toString(),
-        expandedValue: widget.secouristes[1].email.toString(),
-      ),
-    ];*/
-    List<ItemModel> generateItems(int numberOfItems) {
-      return List.generate(numberOfItems, (index) {
-        return ItemModel(
-          headerValue: widget.secouristes[index].name.toString(),
-          expandedValue: widget.secouristes[index].email.toString(),
-          isFree: widget.secouristes[index].isFree,
-        );
-      });
-    }
-
-    List<ItemModel> _data = generateItems(widget.secouristes.length);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.redAccent[700],
@@ -46,140 +24,35 @@ class _ListeSecouristesState extends State<ListeSecouristes> {
         child: ListView.builder(
           itemCount: widget.secouristes.length,
           itemBuilder: (BuildContext context, int index) {
-            return ExpansionPanelList(
-              animationDuration: Duration(milliseconds: 1000),
-              dividerColor: Colors.red,
-              elevation: 1,
-              children: [
-                ExpansionPanel(
-                  body: Container(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          _data[index].expandedValue,
-                          style: TextStyle(
-                              color: Colors.grey[700],
-                              fontSize: 15,
-                              letterSpacing: 0.3,
-                              height: 1.3),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                              padding: EdgeInsets.only(top: 8.0),
-                              child: LiteRollingSwitch(
-                                value: false,
-                                textOn: 'Activer',
-                                textOff: 'Desactiver',
-                                colorOn: Colors.green,
-                                colorOff: Colors.red[700],
-                                iconOn: Icons.done,
-                                iconOff: Icons.alarm_off,
-                                textSize: 13.0,
-                                onChanged: (bool state) {
-                                  print('turned ${(state) ? 'on' : 'off'}');
-                                },
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(
-                                top: 8.0,
-                                bottom: 8.0,
-                              ),
-                              child: LiteRollingSwitch(
-                                value: true,
-                                textOn: 'Admin',
-                                textOff: 'Normal',
-                                colorOn: Colors.green,
-                                colorOff: Colors.red[700],
-                                iconOn: Icons.done,
-                                iconOff: Icons.alarm_off,
-                                textSize: 13.0,
-                                onChanged: (bool state) {
-                                  print('turned ${(state) ? 'on' : 'off'}');
-                                },
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  headerBuilder: (BuildContext context, bool isExpanded) {
-                    return Container(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        _data[index].headerValue,
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
+            return Container(
+              height: 40,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => InterventionParSecouriste(
+                        secouriste: widget.secouristes[index],
                       ),
-                    );
-                  },
-                  isExpanded: _data[index].isExpanded,
-                )
-              ],
-              expansionCallback: (int item, bool status) {
-                setState(() {
-                  _data[index].isExpanded = !_data[index].isExpanded;
-                });
-              },
+                    ),
+                  );
+                },
+                child: Card(
+                  elevation: 3,
+                  child: Text(
+                    widget.secouristes[index].name.toString(),
+                    style: TextStyle(
+                        color: Colors.grey[700],
+                        fontSize: 20,
+                        letterSpacing: 0.3,
+                        height: 1.3),
+                  ),
+                ),
+              ),
             );
           },
         ),
       ),
-      /* body: ExpansionPanelList(
-        expansionCallback: (int index, bool isExpanded) {
-          setState(() {
-            _data[index].isExpanded = !isExpanded;
-          });
-        },
-        // ignore: missing_return
-        children: _data.map<ExpansionPanel>((Item item) {
-          return ExpansionPanel(
-            headerBuilder: (BuildContext context, bool isExpanded) {
-              return ListTile(
-                title: Text(
-                  item.headerValue != null ? item.headerValue : "none",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-              );
-            },
-            body: ListTile(
-              title: Text(item.expandedValue),
-              trailing: Column(
-                children: <Widget>[
-                  
-                  ),
-                 
-                ],
-              ),
-            ),
-            isExpanded: item.isExpanded,
-          );
-        }).toList(),
-      ),*/
     );
   }
-}
-
-class ItemModel {
-  String expandedValue;
-  String headerValue;
-  bool isExpanded;
-  bool isFree;
-
-  ItemModel({
-    this.expandedValue,
-    this.headerValue,
-    this.isExpanded: true,
-    this.isFree,
-  });
 }
