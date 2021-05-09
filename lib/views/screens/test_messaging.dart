bed5d01874d16a149a1af9c25c2849f89f4c163d
souheilbenslama <benslamasouheil@gmail.com>
2021-05-09 20:58:38 +0100

    messaging merged 2

--- lib/views/screens/test_messaging.dart
+++ lib/views/screens/test_messaging.dart
@@ -30,7 +30,7 @@ class _MessageTestState extends State<MessageTest> {
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(16.0))),
           onPressed: () {
-            SocketService.sendMessage("AZE", message.text);
+            SocketService.sendMessage("AZE", message.text, "");
           },
           child: Text("send"),
         ),
